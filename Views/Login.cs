094664b8c913f94fbea70c0c847505d8a690945c
ANGELO AUGUSTO CONTIERO DOS SANTOS WEIMAR <angelo.weimar@alunos.sc.senac.br>
2022-06-22 21:43:16 -0300

    Somehow views was outside from the program main folder, fixed

---
+++ Views/Login.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Windows.Forms;
+using Views.Lib;
+using Controllers;
+
+namespace Views
+{
+    public class Login : BaseForm
+    {
+        FieldForm fieldUsuario;
+        FieldForm fieldSenha;
+		ButtonForm btnConfirmar;
+        ButtonForm btnCancelar;
+
+        public Login() : base("Login", SizeScreen.Small)
+        {
+            fieldUsuario = new FieldForm("Usuário",20,20,260,20);
+            fieldSenha = new FieldForm("Senha",20,100,260,60);
+            fieldSenha.txtField.PasswordChar = '*';
+
+			btnConfirmar = new ButtonForm("Confirmar", 100, 180, this.handleConfirm);
+            btnCancelar = new ButtonForm("Cancelar", 100, 220, this.handleCancel);
+            
+            this.Controls.Add(fieldUsuario.lblField);
+            this.Controls.Add(fieldUsuario.txtField);
+            this.Controls.Add(fieldSenha.lblField);
+            this.Controls.Add(fieldSenha.txtField);
+            this.Controls.Add(btnConfirmar);
+            this.Controls.Add(btnCancelar);
+        }
+
+         private void handleConfirm(object sender, EventArgs e)
+        {
+            try {
+                UsuarioController.Auth(
+                    this.fieldUsuario.txtField.Text,
+                    this.fieldSenha.txtField.Text
+                );
+                (new Menu(this)).Show();
+            } catch (Exception err) {
+                MessageBox.Show(err.Message);
+            }
+        }
+        private void handleCancel(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(" Deseja mesmo sair? ", "Mensage do sistema ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
