840cb197e4ea9f61a85d3711796d947842ef0a96
ANGELO AUGUSTO CONTIERO DOS SANTOS WEIMAR <angelo.weimar@alunos.sc.senac.br>
2022-06-22 21:55:01 -0300

    Added forms libs

---
+++ Views/Lib/Form.cs
@@ -0,0 +1,41 @@
+using System.Windows.Forms;
+
+namespace Views.Lib {
+    public enum SizeScreen
+    {
+        Small,
+        Medium,
+        Large,
+        Different
+    }
+
+
+    public class BaseForm : Form
+    {
+        public BaseForm(
+            string Title,
+            SizeScreen size
+        )
+        {
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            switch (size)
+            {
+                case SizeScreen.Large:
+                    this.ClientSize = new System.Drawing.Size(900, 900);
+                    break;
+                case SizeScreen.Medium:
+                    this.ClientSize = new System.Drawing.Size(600, 600);
+                    break;
+                case SizeScreen.Small:
+                    this.ClientSize = new System.Drawing.Size(300, 300);
+                    break;
+                default:
+                    this.ClientSize = new System.Drawing.Size(300, 900);
+                    break;
+            }
+            this.Text = Title;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+    
+}
